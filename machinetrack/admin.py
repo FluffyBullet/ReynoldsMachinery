8371e6dc5a8d57322ffad449194acec4f0973ef3
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-06-21 22:44:59 +0100

    correct profiles model, include in admin panel with field and filter

--- machinetrack/admin.py
+++ machinetrack/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+
+class ProfileView(admin.ModelAdmin):
+    list_display = (
+        'first_name',
+        'last_name',
+        'company',
+    )
+    list_filter= (
+        'company',
+    )
+
+admin.site.register(Profile, ProfileView)
--- machinetrack/models.py
+++ machinetrack/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
 
-class Profile(models.model):
-    username = models.OneToOneField('User', on_delete=models.CASCADE)
+class Profile(models.Model):
+    username = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=20, blank=True)
     last_name = models.CharField(max_length=30, blank=True)
     company = models.CharField(max_length=30, blank=True)
 
-@receiver(post_save, sender=User)
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+@receiver(post_save, sender=Profile.username)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=Profile.username)
 def save_user_profile(sender, instance, **kwargs):
     instance.profile.save()
 
