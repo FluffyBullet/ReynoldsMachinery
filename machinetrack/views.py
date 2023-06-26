01df3b7263dbd4e9f685f1757f94f6df186d2380
FluffyBullet <103460601+FluffyBullet@users.noreply.github.com>
2023-06-27 00:28:36 +0100

    display alternate screen on homepage if logged in

--- machinetrack/views.py
+++ machinetrack/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Company
 
 # Create your views here.
 
 def home(request):
     template = 'index.html'
     return render(request, template)
+
+def company_overview(request):
+    queryset = Company.objects.all()
